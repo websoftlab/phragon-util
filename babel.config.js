f1a6898e49f9e5b1c125e2d88c74ec44564ac586
GoshaV <gosha@rozaverta.com>
2023-03-17 01:06:07 +0300

    fixed babel.config.js file (for esm support)

--- babel.config.js
+++ babel.config.js
@@ -37,22 +37,35 @@ module.exports = function getBabelConfig(api) {
 				allowDeclareFields: true,
 			},
 		],
-		"babel-plugin-optimize-clsx",
+	];
+
+	if (!api.env("module")) {
+		plugins.push("babel-plugin-optimize-clsx");
+	}
+
+	plugins.push(
 		// Need the following 3 proposals for all targets in .browserslistrc.
 		// With our usage the transpiled loose mode is equivalent to spec mode.
 		["@babel/plugin-proposal-class-properties", { loose: true }],
-		["@babel/plugin-proposal-private-methods", { loose: true }],
-		["@babel/plugin-proposal-object-rest-spread", { loose: true }],
-		["@babel/plugin-proposal-decorators", { loose: true, decoratorsBeforeExport: true }],
-		[
+		["@babel/plugin-proposal-private-methods", { loose: true }]
+	);
+
+	if (!api.env("module")) {
+		plugins.push(["@babel/plugin-proposal-object-rest-spread", { loose: true }]);
+	}
+
+	plugins.push(["@babel/plugin-proposal-decorators", { loose: true, decoratorsBeforeExport: true }]);
+
+	if (!api.env("module")) {
+		plugins.push([
 			"@babel/plugin-transform-runtime",
 			{
 				useESModules,
 				// any package needs to declare 7.4.4 as a runtime dependency. default is ^7.0.0
 				version: "^7.21.0",
 			},
-		],
-	];
+		]);
+	}
 
 	if (api.env(["node", "commonjs"])) {
 		plugins.push([
